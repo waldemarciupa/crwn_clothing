0d56c4fb59f9fda8cc6148758ccbc981c556e46b
waldemarciupa <waldemarciupa90@gmail.com>
2021-03-03 17:20:41 +0100

    created sign in component

--- src/screens/SignInAndSignUp/index.js
+++ src/screens/SignInAndSignUp/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './styles.scss';
+import SignIn from 'components/SignIn';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
-        SIGN IN
+        <SignIn />
     </div>
 )
 
